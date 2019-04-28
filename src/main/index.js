2faa5fa34db5e4d076bd6ecb88818c0f68eabb4c
liuyi <liuyi@seadun.com>
2019-04-28 21:59:14 +0800

    aaa

--- src/main/index.js
+++ src/main/index.js
@@ -2,6 +2,16 @@
 
 import { app, BrowserWindow } from 'electron'
 
+let log4js = require('log4js')
+log4js.configure({
+  appenders: { UI: { type: 'file', filename: 'electron.log' } },
+  categories: { default: { appenders: ['UI'], level: 'info' } }
+})
+
+let logger = log4js.getLogger('UI')
+
+global.logger = logger
+logger.info('日志初始化成功')
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
@@ -22,7 +32,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     height: 300,
     useContentSize: true,
-    width: 400
+    width: 450
   })
 
   mainWindow.loadURL(winURL)
@@ -49,10 +59,10 @@ app.on('activate', () => {
 const puppeteer = require('puppeteer')
 const ipc = require('electron').ipcMain
 let page = null
-
+let browser = null
 ipc.on('create-page', function (event, arg) {
   async function main () {
-    const browser = await puppeteer.launch({headless: true})
+    browser = await puppeteer.launch({headless: false})
     page = await browser.newPage()
     event.sender.send('page-ok', '')
   }
@@ -90,16 +100,61 @@ ipc.on('get-nick-name', function (event, arg) {
   main()
 })
 
+ipc.on('get-address', function (event, arg) {
+  async function main () {
+    await page.setRequestInterception(true)
+    page.on('request', request => {
+      if (request.url().startsWith('https://h5api.m.taobao.com/h5/mtop.taobao.mbis.getdeliveraddrlist')) {
+        logger.info(request.url())
+        logger.info('拦截到了这条url然后就该请求了')
+        page.on('response', response => {
+          if (response.url().startsWith('https://h5api.m.taobao.com/h5/mtop.taobao.mbis.getdeliveraddrlist')) {
+            const req = response.request()
+            logger.info('Response 的:' + req.method, response.status, req.url)
+            let message = response.text()
+            message.then(function (result) {
+              result = result.replace(' mtopjsonp5(', '').replace('})', '}')
+              result = JSON.parse(result)
+              event.sender.send('get-address', JSON.parse(result.data.returnValue))
+            })
+          }
+        })
+        request.continue()
+      } else {
+        request.continue()
+      }
+    })
+    await page.goto('https://member1.taobao.com/member/fresh/deliver_address.htm')
+  }
+  main()
+})
+
 ipc.on('get-goods-info', function (event, arg) {
+  console.log('arg：', arg)
   async function main () {
     await page.goto(arg)
     let goodsInfo = await page.evaluate(() => {
-      let goodsJson = JSON.parse(document.querySelector('#J_DetailMeta > div.tm-clear > script:nth-child(6)').innerText.replace(/\+/g, '').replace(/[ ]/g, '').replace(/[\r\n]/g, '').split('{"skuList":')[1].split(',"defSelected":')[0])
+      let innerText = document.querySelector('#J_DetailMeta > div.tm-clear > script:nth-child(6)').innerText
+      innerText = innerText.substring(innerText.indexOf('{"valItemInfo')).replace('})();', '').replace(');', '')
+      let data = JSON.parse(innerText)
       let goodsImgUrl = document.querySelector('#J_ImgBooth').src
       return {
         goodsImgUrl: goodsImgUrl,
-        goodsJson: goodsJson
+        goodsJson: data
       }
+    })
+    let skuList = goodsInfo.goodsJson.valItemInfo.skuList
+    let skuMap = goodsInfo.goodsJson.valItemInfo.skuMap
+    skuList.forEach(item => {
+      Object.values(skuMap).forEach(value => {
+        if (item.skuId === value.skuId) {
+          item.stock = value.stock
+          item.price = value.price
+        }
+      })
+    })
+    goodsInfo.goodsJson = skuList.filter(item => {
+      return item.stock > 0
     })
     event.sender.send('goods-info-ok', goodsInfo)
   }
@@ -108,16 +163,19 @@ ipc.on('get-goods-info', function (event, arg) {
 
 ipc.on('get-goods-total', function (event, arg) {
   async function main () {
-    let orderMaxNum = await page.evaluate(() => {
-      arg.split(';').forEach(item => {
-        if (document.querySelector(`[data-value='${item}']`).style.display === '') {
-          page.click(`[data-value='${item}']`)
+    let orderMaxNum = await page.evaluate(arg => {
+      let arr = arg.split(';')
+      for (let i in arr) {
+        if (document.querySelector(`[data-value='${arr[i]}']`).style.display === '') {
+          page.click(`[data-value='${arr[i]}']`)
         } else {
-
+          return 0
         }
-      })
-    })
+      }
+      return document.querySelector(`#J_EmStock`).innerHTML
+    }, arg)
     event.sender.send('goods-total-ok', orderMaxNum)
   }
+  // event.sender.send('goods-total-ok', 5)
   main()
 })
--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -7,17 +7,17 @@
         <el-tabs v-model="activeName" @tab-click="handleClick">
           <el-tab-pane label="抢购" name="first">
             <el-row>
-              <el-col :span="20">
+              <el-col :span="21">
                 <el-input placeholder="请输入商品地址" size="mini" v-model="goodsUrl">
                   <el-button slot="append" icon="el-icon-search" @click="searchGoods"></el-button>
                 </el-input>
               </el-col>
-              <el-col :span="4">
+              <el-col :span="3">
                 <img v-if="goodsImgUrl.length>0" :src="goodsImgUrl" width="28px" height="28px"/>
               </el-col>
             </el-row>
             <el-row v-if="goodsImgUrl.length>0">
-              <el-col :span="10">
+              <el-col :span="9">
                 <el-select v-model="orderType" size="mini" filterable clearable placeholder="请选择商品类型" style="width: 100%" @change="handleOrderType">
                   <el-option
                           v-for="item in goodsJson"
@@ -27,15 +27,52 @@
                   </el-option>
                 </el-select>
               </el-col>
-              <el-col :span="10" v-if="orderMaxNum>-1">
-                <el-input-number v-model="orderNum" size="mini" @change="handleChange" :min="0" :max="orderMaxNum" label="描述文字" style="width: 100%"></el-input-number>
+              <el-col :span="5" v-if="orderMaxNum>0">
+                <el-input-number v-model="orderNum" size="mini" @change="handleChange" :min="1" :max="orderMaxNum" label="描述文字" style="width: 100%"></el-input-number>
               </el-col>
-              <el-col :span="4"  v-if="orderMaxNum>-1">
+              <el-col :span="5"  v-if="orderPrice>0">
+                <span style="font-size: 12px;color: #606266;line-height: 28px">价格：{{orderPrice*orderNum}}</span>
+              </el-col>
+              <el-col :span="5"  v-if="orderMaxNum>0">
                 <span style="font-size: 12px;color: #606266;line-height: 28px">库存：{{orderMaxNum}}</span>
               </el-col>
             </el-row>
           </el-tab-pane>
-          <el-tab-pane label="地址" name="second"></el-tab-pane>
+          <el-tab-pane label="地址" name="second">
+            <el-table size="mini"
+                    :data="addressData"
+                    style="width: 100%">
+              <el-table-column
+                      prop="fullName"
+                      label="收货人"
+                      width="80">
+              </el-table-column>
+              <el-table-column
+                      prop="fullAddress"
+                      label="所在地区">
+              </el-table-column>
+              <el-table-column
+                      prop="addressDetail"
+                      label="详细地址">
+              </el-table-column>
+              <el-table-column
+                      prop="provDivisionCode"
+                      label="邮编">
+              </el-table-column>
+              <el-table-column
+                      prop="mobile"
+                      label="电话/手机">
+              </el-table-column>
+              <el-table-column
+                      prop="defaultAddress"
+                      label="">
+                <template slot-scope="scope">
+                  <el-button @click="" v-show="scope.row.defaultAddress === true" type="text" size="small">默认地址</el-button>
+                  <el-button @click="" v-show="scope.row.defaultAddress === false" type="text" size="small">设为默认</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+          </el-tab-pane>
         </el-tabs>
     </div>
   </div>
@@ -52,12 +89,14 @@
         page: null,
         isLogin: false,
         activeName: 'first',
-        goodsUrl: '',
+        goodsUrl: 'https://detail.tmall.com/item.htm?id=589192455077',
         goodsImgUrl: '',
         goodsJson: [],
         orderType: '',
         orderNum: 0,
-        orderMaxNum: -1
+        orderMaxNum: 0,
+        orderPrice: 0,
+        addressData: []
       }
     },
     created () {
@@ -88,6 +127,7 @@
       ipc.on('login-ok', function (event, arg) {
         // 重新登陆
         ipc.send('get-nick-name', '')
+        ipc.send('get-address', '')
       })
 
       ipc.on('nick-name-ok', function (event, arg) {
@@ -103,13 +143,21 @@
       })
 
       ipc.on('goods-total-ok', function (event, arg) {
-        self.orderMaxNum = arg
-        console.log('goods-tota', arg)
+        arg = arg.replace('库存', '').replace('件', '')
+        self.orderMaxNum = parseInt(arg)
+        console.log('goods-total', arg)
+      })
+      ipc.on('get-address', function (event, arg) {
+        console.log('get-address', arg)
+        self.addressData = arg
       })
     },
     methods: {
       handleClick (tab, event) {
         console.log(tab, event)
+        // if (tab.name === 'second') {
+        //   ipc.send('get-address', '')
+        // }
       },
       handleChange (value) {
         console.log(value)
@@ -120,11 +168,12 @@
       },
       handleOrderType (value) {
         const self = this
-        if (value == null) {
-          self.orderMaxNum = -1
-        } else {
-          ipc.send('get-goods-total', value)
-        }
+        self.goodsJson.forEach(item => {
+          if (item.pvs === value) {
+            self.orderMaxNum = item.stock
+            self.orderPrice = item.price
+          }
+        })
       }
     }
   }
